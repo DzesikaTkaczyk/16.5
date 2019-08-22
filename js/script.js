f1fbf6349ac856b358098ed909c31dc4918018f4
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-22 20:34:23 +0200

    changes in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&"
 var prefix = "https://cors-anywhere.herokuapp.com/"
 
 function getQuote() {
-    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&" + randomString();
-    fetch(prefix + quoteUrl, { cache: "no-store" })
+    fetch(prefix + quoteUrl + randomString(), { cache: "no-store" })
         .then(function(resp) {
             return resp.json();
         })
